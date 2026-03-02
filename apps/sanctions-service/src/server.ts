3400ceb0008279f70326373f8ad9d5c2fe8c2fea
juracioliveira <juraci.saocaetano@gmail.com>
2026-03-01 21:56:47 -0300

    fix(sanctions): remove hardcoded pino-pretty transport in production

--- apps/sanctions-service/src/server.ts
+++ apps/sanctions-service/src/server.ts
@@ -11,10 +11,10 @@ dotenv.config();
 
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    transport: {
+    transport: process.env.NODE_ENV !== 'production' ? {
         target: 'pino-pretty',
         options: { colorize: true }
-    }
+    } : undefined
 });
 
 const server = Fastify({
