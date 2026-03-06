5d4cb45a26248527b62d3141b8741770e74014c1
juracioliveira <juraci.saocaetano@gmail.com>
2026-03-06 00:20:15 -0300

    fix(sanctions): remove missing pino-pretty transport from logger

--- apps/sanctions-service/src/utils/logger.ts
+++ apps/sanctions-service/src/utils/logger.ts
@@ -4,9 +4,4 @@ dotenv.config();
 
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
-    ...(process.env.NODE_ENV !== 'production' ? {
-        transport: {
-            target: 'pino-pretty',
-        }
-    } : {})
 });
